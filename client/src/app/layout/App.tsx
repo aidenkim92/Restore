68c0cda690bd11ad0a52441b2d3d7dc72685fdeb
aidenkim92 <dlqnrla@gmail.com>
2023-01-21 20:06:37 +1300

    DONE: DarkMode theme

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,41 +1,32 @@
-import { Container, CssBaseline } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
 import Header from "./Header";
 
 function App() {
-  const [products,setProducts] = useState<Product[]>([]);
+  const [darkMode,setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette:{
+      mode:paletteType,
+      background:{
+        default: paletteType === "light" ? "#eaeaea" : "#121212"
+      }
+    }
+  })
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data));
-  },[]) // adding up the emtpy array dependency here will prevent happening that causes the endless looping - which means it renders only once. 
-  // React redenders when the products list is being added, and the .then(data => setProducts(data)) will add the component and re-render.
-  // Hence, without having the empty array will trigger re-rendering infinitely
-
-  function addProduct(){
-    // setProducts([...products, {name: 'product'+ (products.length + 1), price: (products.length*100)+100}])
-    setProducts(prevState => [...prevState, 
-      {
-        id: prevState.length+101,
-        name: 'product'+ (prevState.length + 1), 
-        price: (prevState.length*100)+100,
-        brand: 'some brand',
-        description: 'some description',
-        pictureUrl: 'http://picsum.photos/200'
-    }])
+  function handleThemeChange(){
+    setDarkMode(!darkMode);
   }
-  
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-       <Catalog products={products} addProduct={addProduct}/>
+       <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,18 +1,23 @@
-import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 
-interface Props{
-    products: Product[];
-    addProduct:() => void;
-}
 
 // Used destructuring by using {products,addProduct} instead of 'props'
-export default function Catalog({products, addProduct}: Props){
+export default function Catalog(){
+    const [products,setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+      fetch('http://localhost:5000/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+    },[]) // adding up the emtpy array dependency here will prevent happening that causes the endless looping - which means it renders only once. 
+    // React redenders when the products list is being added, and the .then(data => setProducts(data)) will add the component and re-render.
+    // Hence, without having the empty array will trigger re-rendering infinitely
+  
     return(
         <>
             <ProductList products={products}/>
-            <Button variant="contained" onClick={addProduct}>Add product</Button>
         </>
     )
 }
