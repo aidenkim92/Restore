9c4d8602c97410880c0f43915e8632782bc79ace
aidenkim92 <dlqnrla@gmail.com>
2023-01-26 20:12:29 +1300

    DONE: Error Handling and Loading window

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 
@@ -6,15 +8,19 @@ import ProductList from "./ProductList";
 // Used destructuring by using {products,addProduct} instead of 'props'
 export default function Catalog(){
     const [products,setProducts] = useState<Product[]>([]);
-
+    const [loading,setLoading] = useState(true);
+    
     useEffect(() => {
-      fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data));
+        agent.Catalog.list().then(products => setProducts(products))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
     },[]) // adding up the emtpy array dependency here will prevent happening that causes the endless looping - which means it renders only once. 
     // React redenders when the products list is being added, and the .then(data => setProducts(data)) will add the component and re-render.
     // Hence, without having the empty array will trigger re-rendering infinitely
   
+    if(loading) return <LoadingComponent message="Loading Product..."/>
+
+
     return(
         <>
             <ProductList products={products}/>
