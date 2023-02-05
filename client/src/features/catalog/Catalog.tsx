03e1382a6ec517c8c78f04f295b78719789dd1bb
aidenkim92 <dlqnrla@gmail.com>
2023-02-05 16:31:53 +1300

    DONE: Redux for basket

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,25 +1,26 @@
-import { useState, useEffect } from "react";
-import agent from "../../app/api/agent";
+import { useEffect } from "react";
 import LoadingComponent from "../../app/layout/LoadingComponent";
-import { Product } from "../../app/models/product";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { fetchProductsAsync, productSelectors } from "./catalogSlice";
 import ProductList from "./ProductList";
 
 
 // Used destructuring by using {products,addProduct} instead of 'props'
 export default function Catalog(){
-    const [products,setProducts] = useState<Product[]>([]);
-    const [loading,setLoading] = useState(true);
+    const products = useAppSelector(productSelectors.selectAll);
+    const {productsLoaded,status} = useAppSelector(state => state.catalog);
+    const dispatch = useAppDispatch();
     
     useEffect(() => {
-        agent.Catalog.list().then(products => setProducts(products))
-        .catch(error => console.log(error))
-        .finally(() => setLoading(false))
-    },[]) // adding up the emtpy array dependency here will prevent happening that causes the endless looping - which means it renders only once. 
+        if(!productsLoaded) dispatch(fetchProductsAsync());
+    },[productsLoaded,dispatch]) 
+    
+    // if the dependency is empty array, then
+    // adding up the emtpy array dependency here will prevent happening that causes the endless looping - which means it renders only once. 
     // React redenders when the products list is being added, and the .then(data => setProducts(data)) will add the component and re-render.
     // Hence, without having the empty array will trigger re-rendering infinitely
   
-    if(loading) return <LoadingComponent message="Loading Product..."/>
-
+    if(status.includes("pending")) return <LoadingComponent message="Loading Product..."/>
 
     return(
         <>
